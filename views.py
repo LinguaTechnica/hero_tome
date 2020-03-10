5aac5b60ff61ffabdd995f2476c7aaa1ca980aa8
K Hudson <dev@lotech.info>
2020-03-10 10:02:45 -0700

    Add signup form and routes.

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
@@ -7,3 +7,22 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+@app.route('/signup')
+def signup_form():
+    """Renders sign-up form"""
+    app.logger.info('Rendering sign-up form ...')
+    return render_template('signup.html')
+
+
+@app.route('/api/signup', methods=['POST'])
+def signup():
+    """
+    Handle signup form.
+    Requires username, email, password and password confirmation.
+    """
+    app.logger.info('Creating new user ...')
+    user_data = request.form
+    app.logger.info(f'User data: {user_data}')
+
+    return redirect('heroes/list.html')
