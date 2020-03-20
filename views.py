559fd024e8593915f4e6cee3ef2266762f8428b1
K Hudson <dev@lotech.info>
2020-03-19 20:38:01 -0700

    Fix signup route and navbar.

--- views.py
+++ views.py
@@ -5,9 +5,15 @@ from models import User
 app = Flask(__name__)
 
 
+# Visitor Routes
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/login')
+def login_form():
+    return render_template('login.html')
 
 
 @app.route('/signup')
@@ -17,38 +23,7 @@ def signup_form():
     return render_template('signup.html')
 
 
-@app.route('/api/signup', methods=['POST'])
-def signup():
-    """
-    Handle signup form.
-    Requires username, email, password and password confirmation.
-    """
-    app.logger.info('Creating new user ...')
-    user_data = request.form
-
-    if user_data.get('password') == user_data.get('passwordConf'):
-        user = User(**user_data)
-        user.create_password(user_data.get('password'))
-        user.save()
-        app.logger.info(f'User {user.id} created. Logging in...')
-        session['user_id'] = user.id
-
-    return redirect('/')
-
-
-@app.route('/users/<int:user_id>')
-def get_user(user_id):
-    user = User.query.get(user_id)
-    app.logger.info(f'Current user: {user}')
-
-    return render_template('users/detail.html', user=user)
-
-
-@app.route('/login')
-def login_form():
-    return render_template('login.html')
-
-
+# Auth Routes
 @app.route('/api/auth', methods=['POST'])
 def login():
     user = User.query.filter_by(username=request.form.get('username')).first()
@@ -69,3 +44,39 @@ def logout():
     del session['user_id']
 
     return redirect('/')
+
+
+@app.route('/api/signup', methods=['POST'])
+def signup():
+    """
+    Handle signup form.
+    Requires username, email, password and password confirmation.
+    """
+    app.logger.info('Creating new user ...')
+    # Create a copy so I can edit the dictionary later
+    user_data = dict(request.form)
+
+    if user_data.get('password') == user_data.get('passwordConf'):
+        # User data must contain only valid fields. Remove invalid
+        del user_data['passwordConf']
+
+        # TODO: Refactor - create utility function that creates users
+        user = User(**user_data)
+        user.create_password(user_data.get('password'))
+        user.save()
+        app.logger.info(f'User {user.id} created. Logging in...')
+        session['user_id'] = user.id
+
+        return redirect(f'/users/{user.id}')
+
+
+# User Routes
+@app.route('/users/<int:user_id>')
+def get_user(user_id):
+    user = User.query.get(user_id)
+    app.logger.info(f'Current user: {user}')
+
+    return render_template('users/detail.html', user=user)
+
+
+# Hero Routes
